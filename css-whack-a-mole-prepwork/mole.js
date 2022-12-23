8b5216ce8022939e47bff1d863093c901bb2f774
JSander72 <jamesearlsanders2@outlook.com>
2022-12-23 14:34:05 -0500

    second commit

--- css-whack-a-mole-prepwork/mole.js
+++ css-whack-a-mole-prepwork/mole.js
@@ -48,10 +48,11 @@ window.addEventListener('DOMContentLoaded', () => {
       clearTimeout(hideTimeout);
       hideMole(event.target);
 
+      // UNCOMMENT THIS LINE OF CODE WHEN DIRECTED
+      // event.target.classList.add('wgs__mole-head--hidden');
 
-      event.target.classList.add('wgs__mole-head--hidden');
-
-      event.target.classList.add('wgs__mole-head--whacked');
+      // UNCOMMENT THIS LINE OF CODE WHEN DIRECTED FOR THE BONUS
+      // event.target.classList.add('wgs__mole-head--whacked');
     });
   }
 });
